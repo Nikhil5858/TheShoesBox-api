87c06551123db8e4d6fcb4ebef5239085c791599
Nikhil5858 <123rathodnikhil@gmail.com>
2023-12-10 17:24:33 +0530

    cart - checkout

---
+++ users/cart.php
@@ -0,0 +1,42 @@
+<?php
+include_once '../database/database.php';
+
+$data = json_decode(file_get_contents('php://input'));
+
+$productId = $data->productId;
+$userId = $data->userId;
+
+if ($productId == "" || $userId == "") {
+    http_response_code(403);
+    $response = ["msg" => "Looks like you missed some fields. Please check and try again!"];
+} else if ($result->num_rows > 0) {
+    $query = "SELECT * FROM cart WHERE user_id = ? and product_id = ?";
+    $params = [$user_id, $product_id];
+    $result = select($query, $params);
+    http_response_code(403);
+    $response = ["msg" => "Product already In Cart. Please choose a different Product."];
+} else {
+    $query = "INSERT INTO cart (user_id,product_id) VALUES(?,?)";
+    $params = [$user_id, $product_id];
+
+    execute($query, $params);
+    $response = ["msg" => "Product Added Successfully"];
+}
+
+
+// Cart Delete
+if ($action == "delete") {
+    $productId = $_POST['productId'];
+    $userId = $_POST['userId'];
+
+
+    if ($productId == '' || $userId == '') {
+        echo "Looks like you missed some fields. Please check and try again!";
+        return;
+    }
+    $sql = "DELETE FROM cart WHERE user_id = '" . $userId . "' AND product_id = '" . $productId . "'";
+    if (mysqli_query($con, $sql)) {
+        echo "Product Delete Successfully";
+        return;
+    }
+}
---
+++ users/checkout.php
@@ -0,0 +1,28 @@
+<?php
+include_once '../database/database.php';
+
+$data = json_decode(file_get_contents('php://input'));
+
+$user_id = $data->user_id;
+$product_id = $data->product_id;
+$address_id = $data->address_id;
+$price = $data->price;
+$size = $data->size;
+$quantity = $data->quantity;
+$totalprice = $data->totalprice;
+
+
+if ($user_id == "" || $product_id == "" || $address_id == "" || $address_id == "" || $price == "" || $size == "" || $quantity == "" || $totalprice == "") {
+    http_response_code(403);
+    $response = ["msg" => "Looks like you missed some fields. Please check and try again"];
+}
+else{
+    $query = "INSERT INTO order (user_id,product_id,address_id,rate,pro_size,quantity,totalprice,status) VALUES(?,?,?,?,?,?,?,?)";
+    $params = [$user_id, $product_id, $address_id, $rate,$pro_size,$quantity,$totalprice,$status];
+    
+    execute($query, $params);
+    $response = ["msg" => "Your Order is successfully added"];
+}
+
+echo json_encode($response)
+?>
