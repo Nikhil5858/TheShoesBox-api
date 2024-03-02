ceaa54e34f19d47473331a81573527eeafcc77b3
Nikhil5858 <123rathodnikhil@gmail.com>
2024-03-02 09:26:57 +0530

    cart

--- cart/increas.php
+++ cart/increas.php
@@ -15,6 +15,6 @@ $response = selectOne($query, $params);
 $cartId =  $response["id"];
 $productQuantity = $response["quantity"];
 
-$newProductQuantity = $productQuantity - 1;
+$newProductQuantity = $productQuantity + 1;
 
 execute("UPDATE cart SET quantity = ? WHERE id = ?", [$newProductQuantity, $cartId]);
